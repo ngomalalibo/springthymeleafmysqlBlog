524d87ed000071306ea217314a18e92ef92b97d5
Ngo <ngomalalibo@yahoo.com>
2020-10-26 21:55:36 +0100

    Initial Commit

--- src/main/java/com/cms/blog/BlogApplication.java
+++ src/main/java/com/cms/blog/BlogApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BlogApplication extends SpringBootServletInitializer
@@ -18,6 +19,23 @@ public class BlogApplication extends SpringBootServletInitializer
     
     public static void main(String[] args)
     {
-        SpringApplication.run(BlogApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(BlogApplication.class, args);
+        /*String[] beanDefinitionNames = run.getBeanDefinitionNames();
+        System.out.println("Beans: " + Arrays.stream(beanDefinitionNames).collect(Collectors.joining(", ", "[", "]")));*/
+        
     }
+    
+    
+    
+    /*@Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder)
+    {
+        return builder.sources(BlogApplication.class);
+    }
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BlogApplication.class, args);
+    }*/
 }
